28de37aa31818e70d23b2dafcbd8b3bdfdb62a6d
coolelvis <elvis911>
2012-10-17 00:04:50 +0800

    g

--- app/admin/events.rb
+++ app/admin/events.rb
@@ -1,3 +1,19 @@
 ActiveAdmin.register Event do
-  
+
+
+  index do
+    column :name
+    column :date_begin
+    column :place
+
+    column "Items"  do |e|
+      e.items.each do |i|
+       p link_to i.title ,item_path(i)
+      end
+
+    end
+    default_actions
+  end
+
+
 end
